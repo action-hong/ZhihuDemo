31cd8b5552a0ef5a20a92ae26a49c8db555fd937
action-hong <kkopitehong@gmail.com>
2016-07-09 00:59:12 +0800

    实现收藏界面里的滑动删除功能

--- app/src/main/java/com/example/kkopite/zhihudemo/activity/FavoriteActivity.java
+++ app/src/main/java/com/example/kkopite/zhihudemo/activity/FavoriteActivity.java
@@ -1,23 +1,25 @@
 package com.example.kkopite.zhihudemo.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
-import android.util.Log;
 
 import com.example.kkopite.zhihudemo.R;
 import com.example.kkopite.zhihudemo.adpter.MyItemTouch;
 import com.example.kkopite.zhihudemo.adpter.NewsAdapter;
 import com.example.kkopite.zhihudemo.db.NewsListDB;
 import com.example.kkopite.zhihudemo.model.NewsBean;
+import com.example.kkopite.zhihudemo.utils.Utils;
 
 import java.util.List;
 
-public class FavoriteActivity extends AppCompatActivity {
+public class FavoriteActivity extends AppCompatActivity implements NewsAdapter.CardClickListener{
 
-    private static final String TAG = "FavoriteActivity";
+    private List<NewsBean> mList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -27,22 +29,22 @@ public class FavoriteActivity extends AppCompatActivity {
         final NewsListDB db = NewsListDB.getInstance(this);
 
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.news_list);
-        final List<NewsBean> mList = db.loadFavourite();
+        mList = db.loadFavourite();
         final NewsAdapter adapter = new NewsAdapter(mList, this);
 
         LinearLayoutManager llm = new LinearLayoutManager(this);
 
         recyclerView.setLayoutManager(llm);
         recyclerView.setAdapter(adapter);
-
+        adapter.setCardClickListener(this);
         MyItemTouch callback = new MyItemTouch(adapter);
         callback.setListener(new MyItemTouch.OnItemMoveListener() {
             @Override
             public void onItemDismiss(int position) {
+                db.deleteFavourite(mList.get(position));
                 mList.remove(position);
                 adapter.notifyItemRemoved(position);
-                db.deleteFavourite(mList.get(position));
-                Log.d(TAG, "onItemDismiss() called with: " + "position = [" + position + "]" + mList.size());
+                adapter.notifyDataSetChanged();
             }
 
             @Override
@@ -53,9 +55,20 @@ public class FavoriteActivity extends AppCompatActivity {
         ItemTouchHelper helper = new ItemTouchHelper(callback);
         helper.attachToRecyclerView(recyclerView);
 
-
-
         adapter.setLoadStatus(NewsAdapter.NOT_SHOW);
+
+    }
+
+    @Override
+    public void onContentClick(int position) {
+        NewsBean bean = mList.get(position);
+        Intent intent = new Intent(this,WebActivity.class);
+        intent.putExtra(Utils.NEWS_BEAN,bean);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onOverflowClick(int position) {
 
     }
 }
